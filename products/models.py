503136ab6017ea7615573c2b2bace0fb7ed4234e
John Murray <jmurray1989@hotmail.com>
2020-10-05 20:03:10 +0000

    Added models, created urls & views for product reviews

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Categories Model
 
@@ -34,3 +35,18 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    """
+    Model to define the fields required to add a review to a product
+    """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,
+                                null=True, blank=True, related_name="reviews")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True,
+                             related_name="reviews")
+    comment = models.TextField(max_length=1000, blank=True, null=True)
+    rating = models.FloatField(default=1)
+
+    def __str__(self):
+        return self.user.username
