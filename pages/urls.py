ccc5e23a7786e07fcd5afec07cd9f8c1ab53a738
John Murray <jmurray1989@hotmail.com>
2020-10-07 09:19:06 +0000

    FAQ template added

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import privacy_policy
+from .views import privacy_policy, faq
 
 urlpatterns = [
-    path('privacy_policy/', privacy_policy, name="privacy_policy")   
+    path('privacy_policy/', privacy_policy, name="privacy_policy"),
+    path('faq/', faq, name="faq"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,10 @@ def privacy_policy(request):
     Renders view for the privacy policy page
     """
     return render(request, "privacy_policy.html")
+
+
+def faq(request):
+    """
+    Renders view for the terms and conditions page
+    """
+    return render(request, "faq.html")
