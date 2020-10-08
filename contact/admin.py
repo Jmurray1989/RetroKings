9150f9b53e96a7c07fc218f9e8f4519e7ee61fbe
John Murray <jmurray1989@hotmail.com>
2020-10-08 20:44:39 +0000

    Contact admin & views set up

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Contact
+
+
+class ContactAdmin(admin.ModelAdmin):
+    # what fields to display
+    list_display = (
+        'name',
+        'user_id',
+        'email',
+        'contact_date',
+    )
+
+    search_fields = (
+        'name',
+        'email',
+    )
+
+    list_per_page = 25
+
+    # orders contacts by date
+    ordering = ('contact_date',)
+
+
+admin.site.register(Contact, ContactAdmin)
--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from .models import Contact
+from .forms import ContactForm
+from django.contrib import messages
+from django.core.mail import send_mail
 
-# Create your views here.
+import os
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+
+
+def contact(request):
+    """
+    Renders contact template and contact
+    form.
+    """
+    if request.method == 'POST':
+        if request.user.is_authenticated:
+            create_contact_form = Contact(
+                name=request.POST.get('name'),
+                message=request.POST.get('message'),
+                email=request.POST.get('email'),
+                user_id=request.user
+            )
+            create_contact_form.save()
+        else:
+            create_contact_form = Contact(
+                name=request.POST.get('name'),
+                message=request.POST.get('message'),
+                email=request.POST.get('email'),
+            )
+            create_contact_form.save()
+
+    context = {
+        'form': ContactForm
+    }
+    return render(request, 'contact/contact.html', context)
