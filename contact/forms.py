2a00ceabab36af6211a933e3f912835e1177f746
John Murray <jmurray1989@hotmail.com>
2020-10-08 20:46:08 +0000

    Contact template & form created

---
+++ contact/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from .models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    """ Contact form """
+    class Meta:
+        model = Contact
+        fields = [
+            'name',
+            'email',
+            'message'
+        ]
+
+    name = forms.CharField(
+        required=True,
+        label='',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Your name'
+        })
+    )
+    message = forms.CharField(
+        required=True,
+        label='',
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'placeholder': 'Your message'
+        })
+    )
+    email = forms.EmailField(
+        required=True,
+        label='',
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Your email'
+        })
+    )
