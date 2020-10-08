ed85495f0cbf19331e63f274fd891cce661f1a7d
John Murray <jmurray1989@hotmail.com>
2020-10-08 19:02:32 +0000

    Created contact models

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=200)
+    message = models.TextField(max_length=200, blank=True)
+    email = models.EmailField(max_length=200)
+    contact_date = models.DateTimeField(default=datetime.now, blank=True)
+    user_id = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
