819dc5e78e3958a82354767acc0f3b0c35e2380b
John Murray <jmurray1989@hotmail.com>
2020-10-08 21:54:52 +0000

    Contact success template, view and url set up

--- contact/views.py
+++ contact/views.py
@@ -10,27 +10,45 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 
 def contact(request):
     """
-    Renders contact template and contact
-    form.
+    Renders contact form.
+
     """
     if request.method == 'POST':
         if request.user.is_authenticated:
-            create_contact_form = Contact(
+            form = Contact(
                 name=request.POST.get('name'),
                 message=request.POST.get('message'),
                 email=request.POST.get('email'),
                 user_id=request.user
             )
-            create_contact_form.save()
+            form.save()
         else:
-            create_contact_form = Contact(
+            form = Contact(
                 name=request.POST.get('name'),
                 message=request.POST.get('message'),
                 email=request.POST.get('email'),
             )
-            create_contact_form.save()
+            form.save()
+
+        # Send Email
+        send_mail(
+            'New Message Received',
+            'Hi Admin,\n\nYou have a new message. Sign '
+            'into the admin panel to view.\n\nRegards,'
+            '\nRetroKings',
+            'retrokingsinc@gmail.com',
+            [EMAIL_HOST_USER],
+            fail_silently=True
+        )
+
+        messages.success(request, 'Contact form successfully submitted. We will be in touch!')
+        return redirect(reverse('contact_success'))
 
     context = {
         'form': ContactForm
     }
     return render(request, 'contact/contact.html', context)
+
+
+def contact_success(request):
+    return render(request, "contact/contact_success.html")
